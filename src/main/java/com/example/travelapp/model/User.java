4669ca7a82bf6ae5f71d3f9dc2d15cdc55eedaa6
ptepmpt <148393456+ptepmpt@users.noreply.github.com>
2025-03-10 02:44:05 +0300

    stuff3

--- src/main/java/com/example/travelapp/model/User.java
+++ src/main/java/com/example/travelapp/model/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,8 +15,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-
 
 @Getter
 @Setter
