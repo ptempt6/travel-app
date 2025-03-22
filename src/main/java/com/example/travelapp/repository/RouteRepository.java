fee45cda0dbf01c0d894bf8531dd1b02bd956abd
ptepmpt <148393456+ptepmpt@users.noreply.github.com>
2025-03-22 17:57:36 +0300

    checkstyle

--- src/main/java/com/example/travelapp/repository/RouteRepository.java
+++ src/main/java/com/example/travelapp/repository/RouteRepository.java
@@ -1,11 +1,11 @@
 package com.example.travelapp.repository;
 
 import com.example.travelapp.model.Route;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface RouteRepository extends JpaRepository<Route, Long> {
     @Query(value = "SELECT r.* FROM Routes r "
